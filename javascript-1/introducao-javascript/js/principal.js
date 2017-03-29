60280412e54250e13491ae545a8743fc32a518a5
hulk <iagogaspar.uema.dpd@gmail.com>
2017-03-29 09:31:12 -0300

    Calculo do imc do primeiro paciente.

--- javascript-1/introducao-javascript/js/principal.js
+++ javascript-1/introducao-javascript/js/principal.js
@@ -1,3 +1,15 @@
 // Obtém o título e troca por outro valor
+//
 var titulo         = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista";
+
+// Busca o primeiro paciente
+//
+var tdPaciente = document.querySelector("#primeiro-paciente");
+var peso       = tdPaciente.querySelector(".info-peso").textContent;
+var altura     = tdPaciente.querySelector(".info-altura").textContent;
+
+var imc   = peso / (altura * altura);
+
+var tdImc         = tdPaciente.querySelector(".info-imc");
+tdImc.textContent = imc;
