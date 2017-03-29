cb4959e022a6fa39f415f4bbdb80a2f691d3883a
hulk <iagogaspar.uema.dpd@gmail.com>
2017-03-29 09:41:55 -0300

    Validação da altura e do peso.

--- javascript-1/introducao-javascript/js/principal.js
+++ javascript-1/introducao-javascript/js/principal.js
@@ -8,8 +8,28 @@ titulo.textContent = "Aparecida nutricionista";
 var tdPaciente = document.querySelector("#primeiro-paciente");
 var peso       = tdPaciente.querySelector(".info-peso").textContent;
 var altura     = tdPaciente.querySelector(".info-altura").textContent;
+var tdImc      = tdPaciente.querySelector(".info-imc");
 
-var imc   = peso / (altura * altura);
+// Validação do peso e da altura
+//
+var alturaEhValida = true;
+var pesoEhValido   = true;
 
-var tdImc         = tdPaciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+if(peso <= 0 || peso > 1000) {
+    console.log("Peso inválido");
+    pesoEhValido      = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if(altura <= 0 || altura > 3) {
+    console.log("Altura inválida");
+    alturaEhValida    = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+// Cálculo do IMC
+//
+if(alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
