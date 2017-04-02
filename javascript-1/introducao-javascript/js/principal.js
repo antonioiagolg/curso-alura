bf88f6ea3f95b85c7c8b88be9c68757e06113044
Antonio Iago Lemos Gaspar <antonio.iagolg@gmail.com>
2017-04-02 10:24:05 -0300

    imc em todos os pacientes

--- javascript-1/introducao-javascript/js/principal.js
+++ javascript-1/introducao-javascript/js/principal.js
@@ -3,33 +3,39 @@
 var titulo         = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista";
 
-// Busca o primeiro paciente
+// Busca todos os pacientes
 //
-var tdPaciente = document.querySelector("#primeiro-paciente");
-var peso       = tdPaciente.querySelector(".info-peso").textContent;
-var altura     = tdPaciente.querySelector(".info-altura").textContent;
-var tdImc      = tdPaciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente")
 
-// Validação do peso e da altura
+// Percorrendo cada paciente
 //
-var alturaEhValida = true;
-var pesoEhValido   = true;
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    var peso     = paciente.querySelector(".info-peso").textContent;
+    var altura   = paciente.querySelector(".info-altura").textContent;
+    var tdImc    = paciente.querySelector(".info-imc");
 
-if(peso <= 0 || peso > 1000) {
-    console.log("Peso inválido");
-    pesoEhValido      = false;
-    tdImc.textContent = "Peso inválido";
-}
+    // Validação do peso e da altura
+    //
+    var alturaEhValida = true;
+    var pesoEhValido   = true;
 
-if(altura <= 0 || altura > 3) {
-    console.log("Altura inválida");
-    alturaEhValida    = false;
-    tdImc.textContent = "Altura inválida";
-}
+    if(peso <= 0 || peso > 1000) {
+        console.log("Peso inválido");
+        pesoEhValido      = false;
+        tdImc.textContent = "Peso inválido";
+    }
 
-// Cálculo do IMC
-//
-if(alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if(altura <= 0 || altura > 3) {
+        console.log("Altura inválida");
+        alturaEhValida    = false;
+        tdImc.textContent = "Altura inválida";
+    }
+
+    // Cálculo do IMC
+    //
+    if(alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
