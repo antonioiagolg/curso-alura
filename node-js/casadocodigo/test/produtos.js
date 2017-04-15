8da930d15522db92a6958ad0bb498dcf93196b35
Antonio Iago Lemos Gaspar <antonio.iagolg@gmail.com>
2017-04-15 08:44:38 -0300

    adicionando teste de listagem em html.

--- node-js/casadocodigo/test/produtos.js
+++ node-js/casadocodigo/test/produtos.js
@@ -2,10 +2,16 @@ var express = require('../config/express.js')();
 var request = require('supertest')(express);
 
 describe('#ProdutosController', function() {
-    it("#Listagem dos produtos", function(done) {
+    it("#Listagem dos produtos json", function(done) {
         request.get('/produtos')
         .set('Accept', 'application/json')
         .expect('content-type',/json/)
+        .expect(200, done);
+    });
+
+    it("#Listagem dos produtos html", function(done) {
+        request.get('/produtos')
+        .set('Accept', 'text/html')
         .expect(200, done);
     });
 });
