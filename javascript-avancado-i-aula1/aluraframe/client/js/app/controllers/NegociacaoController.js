719647891b067abd1b2c51dd11f787099f168fb6
Antonio Iago Lemos Gaspar <iagogaspar@nti.uema.br>
2017-07-26 15:21:10 -0300

    Finalização modulo 1 de javascript avançado

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,9 +4,34 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($("#tabela-negociacoes"));
+        this._negociacoesView.update(this._listaNegociacoes.negociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($("#mensagem-view"));
     }
 
     adiciona(event) {
-        
+
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = "Negociação adicionada com sucesso.";
+        this._mensagemView.update(this._mensagem);
+        this._negociacoesView.update(this._listaNegociacoes.negociacoes);
+
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao() {
+        let data = DataHelper.textoParaData(this._inputData.value);
+        return new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = "";
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0;
+
+        this._inputData.focus();
     }
 }
---
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -0,0 +1,37 @@
+class NegociacoesView extends View {
+    constructor(elemento) {
+        super(elemento);
+    }
+
+    template(model) {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    ${model.map(n =>
+                        `
+                            <tr>
+                                <td>${DataHelper.dataParaTexto(n.data)}</td>
+                                <td>${n.quantidade}</td>
+                                <td>${n.valor}</td>
+                                <td>${n.volume}</td>
+                            </tr>
+                        `
+                    ).join()}
+                </tbody>
+                    <td colspan="3"></td>
+                    <td>${model.reduce((total, n) => total + n.volume, 0)}</td>
+                <tfoot>
+                </tfoot>
+            </table>
+        `;
+    }
+}
