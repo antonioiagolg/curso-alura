829c5e9f253d9417432b08e743bc1bb8430d4646
Antonio Iago Lemos Gaspar <antonio.iagolg@gmail.com>
2017-09-17 10:49:27 -0300

    Aula 06

--- curso-angularjs/public/js/directives/minhas-diretivas.js
+++ curso-angularjs/public/js/directives/minhas-diretivas.js
@@ -4,10 +4,22 @@ angular.module('minhasDiretivas',[])
 
         ddo.estrict = "AE";
         ddo.transclude = true;
-        ddo.templateUrl = 'js/directives/meu-painel.html'
+        ddo.templateUrl = 'js/directives/meu-painel.html';
         ddo.scope = {
             titulo: '@'
         };
         
+        return ddo;
+    })
+    .directive('minhaFoto', () => {
+        let ddo = {};
+
+        ddo.estrict = "AE";
+        ddo.templateUrl = 'js/directives/minha-foto.html';
+        ddo.scope = {
+            url: '@',
+            titulo: '@'
+        };
+
         return ddo;
     });
