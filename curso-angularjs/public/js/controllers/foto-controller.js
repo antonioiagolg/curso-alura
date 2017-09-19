e4e9bbbdacd96518f1ae567d34c2757efeebdefe
marcos.penha <git.12qwaszx>
2017-09-19 15:46:46 -0300

    Aula 08

--- curso-angularjs/public/js/controllers/foto-controller.js
+++ curso-angularjs/public/js/controllers/foto-controller.js
@@ -1,18 +1,38 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http){
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
 
     $scope.foto = {};
     $scope.mensagem = '';
+    var fotoId = $routeParams.fotoId;
+
+    if(fotoId) {
+        $http.get('/v1/fotos/' + fotoId)
+        .then(function(response) {
+            $scope.foto = response.data;
+        }, function(erro){
+            $scope.mensagem = 'Não foi possível obter a foto para edição.';
+        });
+    }
 
     $scope.submeter = function() {
         if($scope.formulario.$valid) {
-            $http.post('/v1/fotos', $scope.foto)
-            .then(function() {
-                $scope.mensagem = 'Imagem incluida com sucesso!';
-                $scope.foto = {};
-            }, function(erro) {
-                $scope.mensagem = 'Houve um problema ao incluir a imagem!';
-                console.log(erro);
-            });
+
+            if(fotoId) {
+                $http.put('/v1/fotos/' + fotoId, $scope.foto)
+                .then(function(response) {
+                    $scope.mensagem = 'Foto alterada com sucesso!';
+                }, function(erro) {
+                    $scope.mensagem = 'Ocorreu um erro ao alterar a foto!';
+                });
+            } else {
+                $http.post('/v1/fotos', $scope.foto)
+                .then(function() {
+                    $scope.mensagem = 'Imagem incluida com sucesso!';
+                    $scope.foto = {};
+                }, function(erro) {
+                    $scope.mensagem = 'Houve um problema ao incluir a imagem!';
+                    console.log(erro);
+                });
+            }
         }
     }
 });
--- curso-angularjs/public/js/controllers/fotos-controller.js
+++ curso-angularjs/public/js/controllers/fotos-controller.js
@@ -2,6 +2,7 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	
 	$scope.fotos = []; 
 	$scope.filtro = '';
+	$scope.mensagem = '';
 
 	$http.get('/v1/fotos')
 	.success(function(retorno) {
@@ -12,4 +13,14 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 		console.log(erro);
 	});
 
+	$scope.remover = function(foto) {
+		$http.delete('/v1/fotos/' + foto._id)
+		.then(function() {
+			$scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+			var indexFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(foto, 1);
+		}, function(erro) {
+			$scope.mensagem = 'Houve um problema ao remover a foto!';
+		});
+	}
 });
