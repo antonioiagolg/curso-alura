58a44a08fcd0e4ec2164e87adeedec4798a6bb11
marcos.penha <git.12qwaszx>
2017-11-07 11:06:51 -0300

    Estudando fetch API

--- javascript-avancado-i-aula1/aluraframe/client/js/app/services/HttpService.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/HttpService.js
@@ -1,39 +1,45 @@
 class HttpService {
 
-    get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4) {
-                    if(xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-            xhr.send();
-        });
+    /**
+     * Trata os erros da requisição
+     * 
+    */
+    _handleErrors(res) {
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
     }
 
+    /**
+     * Busca informações do servidor
+     * @param {String} url A url do recurso
+     * @returns {Promise} Uma promise contendo o resultado
+     * 
+    */
+    get(url) {
+        
+        return fetch(url)
+                .then(res => this._handleErrors(res))
+                .then(res => res.json());
+    }
+
+    /**
+     * Envia informações para o servidor.
+     * 
+     * @param {String} url A url do recurso
+     * @param {Object} dado O dado a ser enviado
+     * @returns {Promise} Uma promise contendo o sucesso ou fracasso da operação
+     * @memberof HttpService
+     */
     post(url, dado) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-Type", "application/json");
 
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-
-            xhr.send(JSON.stringify(dado));
-        });
+        return fetch(url, {
+            method: "post",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res))
+        .then(res => res.json());
     }
 }
